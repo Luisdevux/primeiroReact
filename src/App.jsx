688bcefff9eba47d70038dad2c1f80ba44b6f3d2
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2025-04-30 18:41:33 -0400

    Criando sidebar e espaçamentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from './App.module.css'
+
+import './global.css'
+import { Sidebar } from "./components/Sidebar";
+
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Luis Felipe"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
-      />
-      <Post
-        author="Teste"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Luis Felipe"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+          />
+          <Post
+            author="Teste"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+          />
+        </main>
+      </div>
     </div>
   )
 }
